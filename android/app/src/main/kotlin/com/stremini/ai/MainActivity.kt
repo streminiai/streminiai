b9f24b2ca79d1df6321c56a7d5d50ba70f9958ee
Vishwajeet <vishwajeetadkine705@gmail.com>
2025-11-07 23:23:53 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/stremini/ai/MainActivity.kt
+++ android/app/src/main/kotlin/com/stremini/ai/MainActivity.kt
@@ -1,6 +1,9 @@
-package com.stremini.ai
+// android/app/src/main/kotlin/com/example/stremini_chatbot/MainActivity.kt
+
+package com.example.stremini_chatbot
 
 import io.flutter.embedding.android.FlutterActivity
 
+// Ensure your class extends FlutterActivity directly for v2 embedding.
 class MainActivity: FlutterActivity() {
 }
