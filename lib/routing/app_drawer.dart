d985caa086e80e2c8b4413b087b19845476a0061
Vishwajeet <vishwajeetadkine705@gmail.com>
2025-11-18 10:36:50 +0530

    Update app_drawer.dart

--- lib/routing/app_drawer.dart
+++ lib/routing/app_drawer.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:stremniapp/routing/app_router.dart';
 
 class AppDrawer extends StatelessWidget {
-  const AppDrawer({Key? key}) : super(key: key);
+  const AppDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
