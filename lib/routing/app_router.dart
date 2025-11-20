a419819227921b8c4309d232407bd1b043019105
Vishwajeet <vishwajeetadkine705@gmail.com>
2025-11-20 09:14:17 +0530

    Update app_router.dart

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -4,6 +4,7 @@ import 'package:stremniapp/screens/screen_analyzer_screen.dart';
 import 'package:stremniapp/screens/system_overlay_screen.dart';
 import 'package:stremniapp/screens/custom_keyboard_screen.dart';
 import 'package:stremniapp/screens/chatbot_screen.dart';
+import 'package:stremniapp/screens/settings_screen.dart';
 import 'package:stremniapp/routing/app_drawer.dart';
 
 // Placeholder screens for other features
@@ -12,7 +13,7 @@ class VoiceControlScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: const Text('Voice Control')),
-        drawer: AppDrawer(),
+        drawer: const AppDrawer(),
         body: const Center(child: Text('Voice Control Screen')),
       );
 }
@@ -22,7 +23,7 @@ class AutoTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: const Text('Auto Task')),
-        drawer: AppDrawer(),
+        drawer: const AppDrawer(),
         body: const Center(child: Text('Auto Task Screen')),
       );
 }
@@ -32,12 +33,13 @@ class DigitalBodyguardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: const Text('Digital Bodyguard')),
-        drawer: AppDrawer(),
+        drawer: const AppDrawer(),
         body: const Center(child: Text('Digital Bodyguard Screen')),
       );
 }
 
 class AppRouter {
+  // Route names
   static const String voice = '/voice';
   static const String home = '/home';
   static const String chat = '/chat';
@@ -46,43 +48,58 @@ class AppRouter {
   static const String autoTask = '/auto_task';
   static const String bodyguard = '/bodyguard';
   static const String keyboard = '/keyboard';
+  static const String settings = '/settings';  // NEW
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     Widget screen;
+    
     switch (settings.name) {
       case voice:
         screen = const VoiceControlScreen();
         break;
+        
       case home:
         screen = const HomeScreen();
         break;
+        
       case chat:
         screen = const ChatbotScreen();
         break;
+        
       case analyzer:
         screen = const ScreenAnalyzerScreen();
         break;
+        
       case systemOverlay:
         screen = const SystemOverlayScreen();
         break;
+        
       case autoTask:
         screen = const AutoTaskScreen();
         break;
+        
       case bodyguard:
         screen = const DigitalBodyguardScreen();
         break;
+        
       case keyboard:
         screen = const CustomKeyboardScreen();
         break;
+        
+      case settings:  // NEW
+        screen = const SettingsScreen();
+        break;
+        
       default:
         screen = Scaffold(
           appBar: AppBar(),
-          drawer: AppDrawer(),
+          drawer: const AppDrawer(),
           body: Center(
             child: Text('No route defined for ${settings.name}'),
           ),
         );
     }
-    return MaterialPageRoute(builder: (_) => screen);
+    
+    return MaterialPageRoute(builder: (_) => screen, settings: settings);
   }
 }
