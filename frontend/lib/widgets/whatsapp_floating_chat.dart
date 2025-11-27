574f764a1bfd326d40afcd97190a64d4ab3b345e
Vishwajeet <vishwajeetadkine705@gmail.com>
2025-11-27 08:55:15 +0530

    Update whatsapp_floating_chat.dart

--- frontend/lib/widgets/whatsapp_floating_chat.dart
+++ frontend/lib/widgets/whatsapp_floating_chat.dart
@@ -1,5 +1,3 @@
-// File: frontend/lib/widgets/whatsapp_floating_chat.dart
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../services/api_service.dart';
